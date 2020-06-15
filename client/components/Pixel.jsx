9b15852cf82f5f362f981266d8a30557d2fd8c27
Kim Fitter <26913197+kimnewzealand@users.noreply.github.com>
2020-06-16 11:09:00 +1200

    Add Pixel component with font and back colour

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Pixel = () => {
+  return (
+    <div style ={{
+      fontFamily: 'Times New Roman',
+      backgroundColor:"blue"
+    }}>
+TEST
+    </div>
+  )
+}
+
+export default Pixel
