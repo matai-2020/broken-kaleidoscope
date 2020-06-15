dd5a9c21a47c04a06d74f53ac19fe8401bc29cf7
Kim Fitter <26913197+kimnewzealand@users.noreply.github.com>
2020-06-16 11:51:52 +1200

    Change to this.state.style

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -2,20 +2,17 @@ import React from 'react'
 
 class Pixel extends React.Component {
  state = {
-   fontFamily: 'Times New Roman',
-   backgroundColor: 'blue',
-   height: '100px',
-   width: '100px'
+   style: {
+     fontFamily: 'Times New Roman',
+     backgroundColor: 'blue',
+     height: '100px',
+     width: '100px'
+   }
  }
 
  render () {
    return (
-     <div style ={{
-       fontFamily: this.state.fontFamily,
-       backgroundColor: this.state.backgroundColor,
-       height: this.state.height,
-       width: this.state.width
-     }}>
+     <div style = {this.state.style}>
      </div>
    )
  }
