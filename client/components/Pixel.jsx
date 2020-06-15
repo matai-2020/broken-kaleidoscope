12d5dba45ecb0c04d916782a9cf8588553a91401
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-06-16 11:10:37 +1200

    added pixel with color

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function Pixel() {
-  return <div></div>
+  return <div style={{
+    fontFamily: 'Times New Roman',
+    height: '20px',
+    width: '20px',
+    backgroundColor: 'cornflowerblue'
+  }}></div>
 }
 
 export default Pixel
