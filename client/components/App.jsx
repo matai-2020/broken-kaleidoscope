0c2e6f48a107e8b097518fd9c8d620e2a57b7422
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-06-16 15:16:43 +1200

    Eventful experiments completed

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,17 +6,6 @@ class App extends React.Component {
     fontFamily: 'Times New Roman',
     height: '20px',
     width: '20px',
-    backgroundColor: `cornflowerblue`
-  }
-
-  randomHexColor () {
-    return `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-  }
-
-  clickHandler = (evt) => {
-    this.setState({
-      backgroundColor: this.randomHexColor()
-    })
   }
 
   render () {
@@ -25,23 +14,16 @@ class App extends React.Component {
         <Pixel
           height={this.state.height}
           width={this.state.width}
-          backgroundColor={this.randomHexColor()}
-          clickHandler={this.clickHandler}
         />
 
         <Pixel
           height={this.state.height}
           width={this.state.width}
-          backgroundColor={this.randomHexColor()}
-          clickHandler = {this.clickHandler}
-
         />
 
         <Pixel
           height={this.state.height}
           width={this.state.width}
-          backgroundColor={this.randomHexColor()}
-          clickHandler = {this.clickHandler}
 
         />
 
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,15 +1,50 @@
 import React from 'react'
 
-function Pixel (props) {
-  return (
-    <div style={{
-      height: props.height,
-      width: props.width,
-      backgroundColor: props.backgroundColor
-    }}
-    onClick={props.clickHandler}>
-    </div>
-  )
+class Pixel extends React.Component {
+
+  state ={
+    backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+  }
+
+  clickHandler = (evt) => {
+    this.setState({
+      backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+    })
+  }
+
+  mouseEnter = (evt) => {
+    this.setState({
+      backgroundColor: 'green'
+    })
+  }
+
+  onDoubleClick = (evt) => {
+    this.setState({
+      backgroundColor: 'white'
+    })
+  }
+
+  onDragEnter = (evt) => {
+    this.setState({
+      backgroundColor: 'yellow'
+    })
+  }
+
+  render () {
+    return (
+      <div style={{
+        height: this.props.height,
+        width: this.props.width,
+        backgroundColor: this.state.backgroundColor
+      }}
+      onClick={this.clickHandler}
+      onMouseEnter={this.mouseEnter}
+      onDoubleClick={this.onDoubleClick}
+      onDragEnter={this.onDragEnter}
+      >
+      </div>
+    )
+  }
 }
 
 export default Pixel
