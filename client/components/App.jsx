775d79a940a14fdea6899abc91e5ba4966e9619e
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-06-16 14:30:09 +1200

    added changing colour function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,28 +6,32 @@ class App extends React.Component {
     fontFamily: 'Times New Roman',
     height: '20px',
     width: '20px',
-    backgroundColor: 'cornflowerblue'
+    backgroundColor: `cornflowerblue`
   }
 
-  render () {
+  randomHexColor () {
+    return `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+  }
+
+  render() {
     return (
       <div id="app">
         <Pixel
-          height = {this.state.height}
-          width = {this.state.width}
-          backgroundColor = {this.state.backgroundColor}
+          height={this.state.height}
+          width={this.state.width}
+          backgroundColor={this.randomHexColor()}
         />
 
         <Pixel
-          height = {this.state.height}
-          width = {this.state.width}
-          backgroundColor = {this.state.backgroundColor}
+          height={this.state.height}
+          width={this.state.width}
+          backgroundColor={this.randomHexColor()}
         />
 
         <Pixel
-          height = {this.state.height}
-          width = {this.state.width}
-          backgroundColor = {this.state.backgroundColor}
+          height={this.state.height}
+          width={this.state.width}
+          backgroundColor={this.randomHexColor()}
         />
 
       </div>
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-function Pixel (props) {
-  return <div style={{
-    height: props.height,
-    width: props.width,
-    backgroundColor: props.backgroundColor
-  }}>
-
-  </div>
+function Pixel(props) {
+  return (
+    <div style={{
+      height: props.height,
+      width: props.width,
+      backgroundColor: props.backgroundColor
+    }}>
+    </div>
+  )
 }
 
 export default Pixel
