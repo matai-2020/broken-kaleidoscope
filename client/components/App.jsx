895f4b5ce5b5820ee7c87a812d066d6840aae495
Rikke-Jorgensen <rikke4800@hotmail.com>
2020-06-17 13:29:12 +1200

    empty Div Pixels added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Pixels from './Pixels'
 
 const App = () => {
   return (
+    <>
     <div>React development has begun!</div>
+    <Pixels />
+    </>
   )
 }
 
---
+++ client/components/Pixels.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pixels = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Pixels
